95000f35bddaa73a19255f4a1f9b6995df004335
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:40 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/predicate.js
+++ js/dist/predicate.js
@@ -209,10 +209,10 @@
 	if (typeof exports === "object") {
 		definition(exports);
 	} else if (typeof define === "function" && define.amd) {
-		define("aureooms-js-predicate", [], function () {
+		define("@aureooms/js-predicate", [], function () {
 			return definition({});
 		});
 	} else if (typeof window === "object" && typeof window.document === "object") {
 		definition(window["predicate"] = {});
-	} else console.error("unable to detect type of module to define for aureooms-js-predicate");
+	} else console.error("unable to detect type of module to define for @aureooms/js-predicate");
 })();
